ead31c3c4fa6919eff105b580df2290b9007f027
AlexGarcia <afgarcia666@gmail.com>
2019-12-04 10:48:29 -0500

    started js and created varibles

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("simon says")
+//
+// 1. Create varibles for start button, level counter and color ids
+let start = document.querySelector(".start");
+let level = document.querySelector(".level");
+let red = document.querySelector("#red");
+let yellow = document.querySelector("#yellow");
+let green = document.querySelector("#green");
+let blue = document.querySelector("#blue");
