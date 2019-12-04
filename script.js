0311f4cb45798d7210a36d971f70bb3c6974e056
AlexGarcia <afgarcia666@gmail.com>
2019-12-04 10:58:56 -0500

    added colors array to js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 console.log("simon says")
 //
-// 1. Create varibles for start button, level counter and color ids
+// 1. Create varibles for start button, level counter, color ids and colors array
 let start = document.querySelector(".start");
 let level = document.querySelector(".level");
 let red = document.querySelector("#red");
 let yellow = document.querySelector("#yellow");
 let green = document.querySelector("#green");
 let blue = document.querySelector("#blue");
+let colors = document.querySelectorAll(".colors")
+
+console.log(colors);
